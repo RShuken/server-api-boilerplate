6426e7e1822a5519ed56556af52c3cffee207cc8
rshuken <ryanshuken@gmail.com>
2020-10-30 14:55:04 -0600

    added a 404 error when a bookmark does not exist for all routes

--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -47,28 +47,44 @@ bookmarksRouter
       .catch(next);
   });
 
-bookmarksRouter.route('/:bookmark_id').get((req, res, next) => {
-  const knexInstance = req.app.get('db');
-  BookmarkService.getById(knexInstance, req.params.bookmark_id)
-    .then((bookmark) => {
-      if (!bookmark) {
-        return res.status(404).json({
-          error: { message: 'Bookmark doesn\'t exist' },
-        });
-      }
-      res.json(serializeBookmark(bookmark));
-    })
-    .catch(next);
-}).delete((req, res, next) => {
-  BookmarkService.deleteBookmark(
-    req.app.get('db'),
-    req.params.bookmark_id
-  )
-    .then(() => {
-      res.status(204).end();
-    })
-    .catch(next);
-});
+bookmarksRouter
+  .route('/:bookmark_id')
+  .all((req, res, next) => {
+    BookmarkService.getById(
+      req.app.get('db'),
+      req.params.bookmark_id
+    )
+      .then(bookmark => {
+        if (!bookmark) {
+          return res.status(404).json({
+            error: { message: 'Bookmark doesn\'t exist' }
+          });
+        }
+        res.bookmark = bookmark; // save the bookmark for the next middleware
+        next(); // don't forget to call next so the next middleware happens!
+      })
+      .catch(next);
+  })
+  .get((req, res, next) => {
+    res.json({
+      id: res.bookmark.id,
+      title: xss(res.bookmark.title),
+      url: xss(res.bookmark.url),
+      description: xss(res.bookmark.description),
+      rating: parseInt(xss(res.bookmark.rating)),
+      date_added: res.bookmark.date_added,
+    });
+  })
+  .delete((req, res, next) => {
+    BookmarkService.deleteBookmark(
+      req.app.get('db'),
+      req.params.bookmark_id
+    )
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
 
 
 module.exports = bookmarksRouter;
